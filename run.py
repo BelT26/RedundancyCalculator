a6bfd499c789705707c6b1a18e778de780f56c96
BelT26 <helen.taylor@hotmail.it>
2021-08-29 12:12:24 +0000

    add functions to view and authorise redundancy applications

--- run.py
+++ run.py
@@ -16,9 +16,6 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 SHEET = GSPREAD_CLIENT.open('Redundancy Applications')
 
-applications = SHEET.worksheet('applications')
-
-data = applications.get_all_values()
 
 print('Welcome to the Miki Travel Voluntary redundancy calculator.')
 
@@ -53,15 +50,68 @@ def check_password():
         password = input('Please enter your password: ')
         if password == '#MTL':
             print('correct password')
-            break
+            return True
         else:
             attempts -= 1
             print('incorrect password')
     print('Password attempts exhausted')
+    return False
+
+pending = SHEET.worksheet('applications').get_all_values()
+headings = pending[0]
+first_appl = pending[1]
+approved = SHEET.worksheet('approved')
+
+def view_pending():
+    global pending
+    global headings
+    global first_appl
+    for head, app in zip(headings, first_appl):
+        print(f"{head}: {app}")
+    print('Do you wish to approve this application?')
+    approve = input('Please enter Y or N: ')
+    if approve.lower() == 'y':
+        authorise(first_appl)
+    else:
+        print('Do you wish to reject this application?')
+        reject = input('Please enter Y or N')
+        if reject.lower() == 'y':
+            reject(first_appl)
+
+
+def authorise(data):
+    global approved
+    print('Authorising application.')
+    approved.append_row(data)
+    pending.batch_clear(first_appl)
+
+
+def reject(data):
+    print('Rejecting application.')
+    rejection_worksheet = SHEET.worksheet('rejected')
+    rejection_worksheet.append_row(data)
+
+
+
 
 
 if access_level == 'admin':
-    check_password()
+    if check_password():
+        pending = SHEET.worksheet('applications').get_all_values()
+        num_pending = len(pending)-1
+        print(f'{num_pending} application(s) pending approval')
+        print('Do you wish to view the pending application(s)?')
+        while True:
+            view = input('Please enter Y or N')
+            if view.lower() == 'y':
+                print('First pending application:')
+                view_pending()
+                break
+            elif apply.lower() == 'n':
+                print('Would you like to access other data?')
+                break
+            else:
+                print('You must enter Y or N')
 
 
 def get_gross_salary():
@@ -153,9 +203,9 @@ def calculate_statutory(age, years_service, weekly_pay):
             std_rate_yrs = statutory_years - high_rate_yrs
         else:
             std_rate_yrs = min(41 - 22, statutory_years - low_rate_yrs)
-    print(f'lower rate years: {low_rate_yrs}')
-    print(f'standard years: {std_rate_yrs}')
-    print(f'higher rate years: {high_rate_yrs}')
+    # print(f'lower rate years: {low_rate_yrs}')
+    # print(f'standard years: {std_rate_yrs}')
+    # print(f'higher rate years: {high_rate_yrs}')
     years = (low_rate_yrs * 0.5) + std_rate_yrs + (high_rate_yrs * 1.5)
     stat_pay = weekly_stat * years
     return stat_pay
@@ -168,7 +218,7 @@ def calculate_pay_in_lieu(salary, years_service):
     if the years worked are less than 5 defaults to 1 month's pay
     """
     if years_service > 4:
-        return (salary / 52) * years_service
+        return round((salary / 52) * years_service, 2)
     return round((salary / 12), 2)
 
 
@@ -242,7 +292,12 @@ def calculate_overtime_payment(salary):
     get_overtime_minutes functions.
     the standard working week is 37.5 hours
     """
-    total_time = get_overtime_hours() + get_overtime_minutes()
+    hours = get_overtime_hours()
+    if hours == 75:
+        minutes = 0
+    else:
+        minutes = get_overtime_minutes()
+    total_time = hours + minutes
     overtime_payment = salary / (52*37.5) * total_time
     return overtime_payment
 
@@ -252,45 +307,82 @@ def calculate_tax(salary, overtime, pay_in_lieu, holidays):
     higher_rate_tax = 0
     highest_rate_tax = 0
     taxable_sum = (salary/12) + overtime + pay_in_lieu + holidays - (12570/12)
-    if taxable_sum < (37700/12):
+    if taxable_sum < 0:
+        return 0
+    elif taxable_sum < (37700/12):
         standard_rate_tax = taxable_sum * 0.2
         return standard_rate_tax
-    standard_rate_tax = (37700/12) * 0.2
-    if taxable_sum < (137430/12):
-        higher_rate_tax = (taxable_sum - 37700/12) * 0.4
-        return standard_rate_tax + higher_rate_tax
-    higher_rate_tax = (99730/12) * 0.4
-    highest_rate_tax = (taxable_sum - 137430/12) * 0.45
-    return standard_rate_tax + higher_rate_tax + highest_rate_tax
+    else:
+        standard_rate_tax = (37700/12) * 0.2
+        if taxable_sum < (137430/12):
+            higher_rate_tax = (taxable_sum - 37700/12) * 0.4
+            return standard_rate_tax + higher_rate_tax
+        else:
+            higher_rate_tax = (99730/12) * 0.4
+            highest_rate_tax = (taxable_sum - 137430/12) * 0.45
+            return standard_rate_tax + higher_rate_tax + highest_rate_tax
+
+
+staff_data = []
 
 
 def calculate_redundancy():
     los = get_length_of_service()
     if los < 2:
-        print('You must have completed at least 2 years for redundancy')
-        return
+        print('You must have completed at least 2 years for redundancy.')
+        exit()
     gross_salary = get_gross_salary()
     weekly_salary = gross_salary/52
-    rounded_weekly = round(weekly_salary, 2)
-    vol_ex = calculate_voluntary_extra(los, rounded_weekly)
+    vol_ex = round(calculate_voluntary_extra(los, weekly_salary), 2)
     staff_age = get_age()
-    statutory = calculate_statutory(staff_age, los, rounded_weekly)
+    statutory = round(calculate_statutory(staff_age, los, weekly_salary), 2)
     lieu = calculate_pay_in_lieu(gross_salary, los)
     num_hols = calculate_holidays(los)
     holiday_pay = calculate_holiday_pay(num_hols, gross_salary)
     overtime = round(calculate_overtime_payment(gross_salary), 2)
     tax = round(calculate_tax(gross_salary, overtime, lieu, holiday_pay), 2)
-    std_red = statutory + lieu + holiday_pay + overtime - tax
-    vol_red = statutory + vol_ex
-    print(f'Your ex gratia payment for voluntary redundance is {vol_ex}\n')
-    print(f'Your statutory redundancy is {statutory} \n')
-    print(f'Your pay in lieu of notice is {lieu} \n')
-    print(f'Your payment for unused holidays is {holiday_pay}')
-    print(f'Your payment for overtime is {overtime}\n')
-    print(f'Total tax deductions are {tax}\n')
+    std_red = round(statutory + lieu + holiday_pay + overtime - tax, 2)
+    vol_red = std_red + vol_ex
+    print(f'\nEx gratia payment for voluntary redundancy: {vol_ex}')
+    print(f'Statutory redundancy: {statutory}')
+    print(f'Pay in lieu of notice: {lieu}')
+    print(f'Unused holidays: {holiday_pay}')
+    print(f'Overtime: {overtime}')
+    print(f'Total tax deductable: {tax}\n')
     print(f'You would receive {vol_red} for voluntary redundancy.')
-    print(f'Your non-voluntary redundancy payment would be {std_red}.')
+    print(f'Your non-voluntary redundancy payment would be {std_red}.\n')
+    global staff_data
+    staff_data.extend((gross_salary, statutory, vol_ex, lieu, holiday_pay, overtime, tax, vol_red, 'pending'))
+
+
+def check_if_applying():
+    print('Do you wish to proceed with your application?')
+    while True:
+        apply = input('Please enter Y or N: ')
+        if apply.lower() == 'y':
+            print('Processing application')
+            return True
+        elif apply.lower() == 'n':
+            print('Application not processed')
+            return False
+        else:
+            print('You must enter Y or N')
+
+
+def update_applications_worksheet(data):
+    print('Updating worksheet')
+    applications_worksheet = SHEET.worksheet('applications')
+    applications_worksheet.append_row(data)
 
 
 if access_level == 'basic':
+    print('Please ensure you have your payroll number and access to your')
+    print('time and attendance records. \n')
     calculate_redundancy()
+    application = check_if_applying()
+    if application:
+        name = input('Please enter your full name: ')
+        department = input('Please enter your department: ')
+        staff_data.insert(0, name)
+        staff_data.insert(1, department)
+        update_applications_worksheet(staff_data)
